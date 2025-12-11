237e67a3f789e5231c9343dd06e1d6825469d863
Sammylee <sammul95@gmail.com>
2025-12-11 15:19:25 +0900

    feat: 프로젝트 초기 설정(#3) (#4)
    * chore: 초기 세팅 진행(#3)
    
    - 당장 사용하지 않는 폴더 삭제
    - 디자인토큰 설정(컬러, 폰트)
    
    * chore: 라우팅 설정(#3)
    
    - 라우터 설정 추가
    - 404 페이지, 메인페이지 추가
    
    * feat: cn 유틸 추가(#3)
    
    - cn 유틸리티 함수 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import './App.css'
+import MainLayout from '@components/layout/MainLayout'
+import { ROUTES_PATHS } from '@constants'
+import { MainPage, NotFound } from '@pages'
+import { Route, Routes } from 'react-router'
 
 function App() {
-  return <div>template</div>
+  const ROUTES = [
+    {
+      path: ROUTES_PATHS.MAIN,
+      element: <MainPage />,
+    },
+    {
+      path: ROUTES_PATHS.NOT_FOUND,
+      element: <NotFound />,
+    },
+  ]
+
+  return (
+    <Routes>
+      <Route element={<MainLayout />}>
+        {ROUTES.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+      </Route>
+    </Routes>
+  )
 }
 
 export default App
