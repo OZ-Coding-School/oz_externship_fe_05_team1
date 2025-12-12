4d340bdfd99c7b685a80ba9d489642f68e0e4362
LeeSaemMool <sammul95@gmail.com>
2025-12-13 02:56:01 +0900

    feat: side menu 구현(#8)
    - SideMenu 컴포넌트 추가(아코디언 메뉴)
    - 메뉴 데이터 분리(SideMenuData.ts)
    - 아이콘 추가(User, Book, Angleup, AngleDown)
    - MainLayout에 SideMenu 연결
    - 배럴패턴 추가
    - SideMenu map 내부 구조분해 적용

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,5 +2,12 @@
 /// <reference types="vite-plugin-svgr/client" />
 
 export { default as ProfileIcon } from './icon/profile-icon.svg?react'
+<<<<<<< HEAD
 export { default as CalendarIcon } from './icon/calendar-icon.svg?react'
 export { default as UpsideDownTriangle } from './icon/upsideDown-triangle-icon.svg?react'
+=======
+export { default as UserIcon } from './icon/user-icon.svg?react'
+export { default as BookIcon } from './icon/book-icon.svg?react'
+export { default as AngleUpIcon } from './icon/angle-up.svg?react'
+export { default as AngleDownIcon } from './icon/angle-down.svg?react'
+>>>>>>> 66c95a0 (feat: side menu 구현(#8))
