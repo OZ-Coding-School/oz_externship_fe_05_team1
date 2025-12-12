5e97ece88dd0528b91c17af3b7353f6291590257
fortes42-lgtm <forteslv42@gmail.com>
2025-12-12 15:11:56 +0900

    refactor: 버튼 컴포넌트 리팩토링(#12)
    - ButtonType.tsx 파일 Button.tsx에 포함
    - Button에 직접 객체 포함

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -1,23 +1,34 @@
-import { cn } from '@utils/cn'
+import type { VariantProps } from 'class-variance-authority'
 
-import type { ButtonProps } from './ButtonType'
+import { cn } from '@utils/cn'
 
 import { buttonVariants } from './ButtonStyle'
 
-/** Button Component
- * @param variant - Button Style ('primary' | 'danger' | 'success' | 'outline')
- * @param size - Button Size ('sm' | 'md' | 'lg' | 'xl')
- * @param children - Button Content
+export type ButtonProps = React.ComponentPropsWithoutRef<'button'> &
+  VariantProps<typeof buttonVariants> & {
+    children: React.ReactNode
+  }
+
+/**
+ * 버튼 컴포넌트
+ * @param variant - 버튼 스타일 ('primary' | 'danger' | 'success' | 'outline')
+ * @param size - 버튼 크기 ('sm' | 'md' | 'lg' | 'xl')
+ * @param children - 버튼 내용
  */
-
-export default function Button(props: ButtonProps) {
-  const { className, variant, size, children, ...rest } = props
-
+export default function Button({
+  className,
+  variant,
+  size,
+  children,
+  disabled,
+  type,
+  ...rest
+}: ButtonProps) {
   return (
     <button
-      className={cn(buttonVariants({ variant, size, className }))}
-      disabled={props.disabled}
-      type={props.type || 'button'}
+      className={cn(buttonVariants({ variant, size }), className)}
+      disabled={disabled}
+      type={type || 'button'}
       {...rest}
     >
       {children}
