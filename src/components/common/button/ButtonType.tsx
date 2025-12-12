0386981899667d01b416a25ac757d65d06f335bf
fortes42 <forteslv42@gmail.com>
2025-12-12 14:57:04 +0900

    refactor: 버튼 컴포넌트 리팩토링(#12)
    - ButtonType.tsx 파일 Button.tsx에 포함
    - Button에 직접 객체 포함

--- src/components/common/button/ButtonType.tsx
+++
@@ -1,8 +0,0 @@
-import type { VariantProps } from 'class-variance-authority'
-
-import { buttonVariants } from './ButtonStyle'
-
-export type ButtonProps = React.ComponentPropsWithoutRef<'button'> &
-  VariantProps<typeof buttonVariants> & {
-    children: React.ReactNode
-  }
