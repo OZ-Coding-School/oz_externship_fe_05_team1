6c9f30892189377e1d52cf1016dd0f28e301c54d
LeeSaemMool <sammul95@gmail.com>
2025-12-12 13:33:54 +0900

    Revert "feat: 헤더구현(#6)"
    This reverts commit d46b6fddffab74d858e9abae8850674448dbe0a8.

--- src/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as MainPage } from './layout/MainLayout'
-export { default as Header } from './layout/Header'
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import svgr from 'vite-plugin-svgr'
-
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
@@ -16,9 +14,6 @@ export default defineConfig({
       '@api': path.resolve(__dirname, './src/api'),
       '@constants': path.resolve(__dirname, './src/constants'),
       '@assets': path.resolve(__dirname, './src/assets'),
-      '@stores': path.resolve(__dirname, './src/stores'),
-      '@features': path.resolve(__dirname, './src/features'),
-      '@custom-types': path.resolve(__dirname, './src/types'),
     },
   },
 })
