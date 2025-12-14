74f3aee2706d66c0b1391d3aebdf7a5bfea1303a
웅배의 깃헙 <jeonwb622@gmail.com>
2025-12-14 09:45:04 +0900

    feat: Date Input구현 (#18)
    * feat: date-input 구현(#17)
    
    - DatePicker 라이브러리 도입
    - DateInput 컴포넌트 구현 (react-datepicker 사용)
    - 캘린더 및 삼각형 SVG 아이콘 추가 및 assets 인덱스 반영
    - 날짜 포맷, 기본값 처리 및 스타일링 적용
    
    * feat: customtime-input 생성(#17)
    
    - DatePicker 사용하여 시간 인풋 구현
    - CustomDateInput, CustomTimeInput 로직 분리
    
    * refactor: 타입 수정 및 date/time 입력 컴포넌트 리팩토(#17)
    
    - CustomDateInput, CustomTimeInput, DateInput의 타입 정의 수정
    - forwardRef 코드 변경

---
+++ src/components/common/input/CustomTimeInput.tsx
@@ -0,0 +1,31 @@
+import { UpsideDownTriangle } from '@assets'
+import React from 'react'
+
+export type CustomTimeInputProps = {
+  value?: string
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
+  ref?: React.Ref<HTMLButtonElement>
+}
+
+/**
+ * 커스텀된 TimeInput을 반환함
+ */
+export default function CustomTimeInput({
+  value,
+  onClick,
+  ref,
+}: CustomTimeInputProps) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      ref={ref}
+      className="flex w-[100px] items-center rounded-md border border-neutral-200 px-3 py-2 text-left"
+    >
+      <span className="flex-1">{value}</span>
+      <UpsideDownTriangle className="relative h-4 w-4 text-neutral-400" />
+    </button>
+  )
+}
+
+CustomTimeInput.displayName = 'CustomTimeInput'
