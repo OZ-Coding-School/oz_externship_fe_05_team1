b309283fe0160f274e8631db5edc906b176914c3
fortes42-lgtm <forteslv42@gmail.com>
2025-12-17 17:32:55 +0900

    feat: 커스텀 토스트 메시지 시스템 구현(#40)
    주요 변경 사항 (key changes)
    - 커스텀 UI 컴포넌트 (toastmessage) 구현
    - 색상별 아이콘 독립 임포트
    - typescript 타입 안정성 확보
    - 전역 유틸리티 함수 (showtoast)
    
    작업 상세 (Technical Details)
    - headless 방식 적용
    - 조건부 렌더링 로직 분리
    - ux 개선

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import MainLayout from '@components/layout/MainLayout'
 import { ROUTES_PATHS } from '@constants'
 import { MainPage, NotFound } from '@pages'
+import { Toaster } from 'react-hot-toast'
 import { Route, Routes } from 'react-router'
 
 function App() {
@@ -16,13 +17,32 @@ function App() {
   ]
 
   return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        {ROUTES.map((route) => (
-          <Route key={route.path} path={route.path} element={route.element} />
-        ))}
-      </Route>
-    </Routes>
+    <>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        gutter={8}
+        containerStyle={{
+          top: 20,
+          right: 20,
+        }}
+        toastOptions={{
+          duration: 4000,
+          style: {
+            padding: '0',
+            background: 'transparent',
+            boxShadow: 'none',
+          },
+        }}
+      />
+      <Routes>
+        <Route element={<MainLayout />}>
+          {ROUTES.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
+        </Route>
+      </Routes>
+    </>
   )
 }
 
