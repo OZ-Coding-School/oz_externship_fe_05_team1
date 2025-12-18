a1307e0cf575b88e6034c606f8ca6877f55fba5f
LeeSaemMool <sammul95@gmail.com>
2025-12-18 13:21:30 +0900

    refactor: button 컴포넌트 스타일 코드 개선
    - Tailwind 클래스 오타 수정
    - 하드코딩된 색상값을 디자인토큰으로 변경
    - defaultVariants 위치 수정
    
    **브랜치 변경해서 다시 커밋
    
    - 추가수정: index.ts 경로수정, PopupModal.tsx 수정된 버튼스타일 반영, 코드충돌 해결

--- src/components/common/button/buttonStyle.ts
+++ src/components/common/button/buttonStyle.ts
@@ -1,36 +1,33 @@
 import { cva } from 'class-variance-authority'
 
 export const buttonVariants = cva(
-  'rounded-md font-medium transition-colors duration-150 flex justify-center items-center disabled: opacity-50 disabled: cursor-not-allowed',
+  'rounded-md font-medium font-normal transition-colors duration-150 flex justify-center items-center px-3.5 py-1.5 text-[16px] disabled:opacity-50 disabled:cursor-not-allowed',
   {
     variants: {
       variant: {
         primary:
-          'bg-[#7C35D9] text-white hover:bg-[#6c2da6] focus: ring-4 focus: ring-[#7C35D9]/50',
+          'bg-primary-400 text-white hover:bg-primary-500 hover:text-primary-400',
+        'primary-light':
+          'bg-primary-light text-primary-500 hover:bg-primary-100',
+        secondary: 'bg-neutral-300 text-white hover:bg-neutral-400',
+        'white-outline':
+          'bg-bg-primary border border-neutral-200 text-neutral-500 hover:bg-neutral-300 hover:text-white',
         success:
-          'bg-[#5EB669] text-white hover:bg-[#4d9255] focus:ring-4 focus:ring-[#5EB669]/50',
-        danger:
-          'bg-[#CC0A0A] text-white hover:bg-[#a3080a] focus:ring-4 focus:ring-[#CC0A0A]/50',
-        'outline-primary':
-          'border border-[#7C35D9] bg-transparent text-[#7C35D9] hover:bg-[#7C35D9] hover:text-white',
-        'outline-danger':
-          'border border-[#CC0A0A] bg-transparent text-[#CC0A0A] hover:bg-[#CC0A0A] hover:text-white',
-        default:
-          'bg-white border border-[#DDDDDD] text-[#666666] hover:bg-gray-50 focus:ring-4 focus:ring-gray-200/50',
-        'outline-success': 'bg-[#E7F4E9] text-[#5EB669] hover:bg-[#d8e8dc]',
-        'ghost-primary': 'bg-[#EDE6FF] text-[#7C35D9] hover:bg-[#d5cbf2]',
-        'disabled-input': 'bg-[#EDE6FF] text-gray-400 cursor-not-allowed',
+          'bg-success text-white hover:bg-success-light hover:text-success',
+        danger: 'bg-error text-white hover:bg-error-light hover:text-error',
+        'success-light':
+          'bg-success-light text-success hover:bg-success hover:text-success-light',
       },
       size: {
-        sm: 'px-3 py-1 text-sm',
-        md: 'px-4 py-2 text-sm',
-        lg: 'px-6 py-3 text-sm',
-        xl: 'w-full px-8 py-5 text-sm',
+        sm: 'w-[48px] h-[24px] text-[14px]',
+        md: 'w-[64px] h-[36px]',
+        lg: 'w-[72px] h-[36px]',
+        xl: 'w-[100px] h-[36px]',
       },
-      defaultVariants: {
-        variant: 'primary',
-        size: 'md',
-      },
+    },
+    defaultVariants: {
+      variant: 'primary',
+      size: 'md',
     },
   }
 )
