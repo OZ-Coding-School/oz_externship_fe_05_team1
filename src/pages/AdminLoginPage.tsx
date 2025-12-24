740b469c65b096e1513ce82b650ccb950d29a5ad
Sammylee <sammul95@gmail.com>
2025-12-24 10:31:19 +0900

    feat: 어드민 로그인 페이지 ui 구현(#81)
    - AdminLoginPage 컴포넌트 생성
    - 이메일/비밀번호 입력 폼 구현
    - 라우팅 설정
    - 배럴패턴 적용

---
+++ src/pages/AdminLoginPage.tsx
@@ -0,0 +1,52 @@
+import { OzcodingLogo } from '@assets'
+import { BaseInput, Button, Image } from '@components'
+import { cn } from '@utils'
+import { useState } from 'react'
+
+export default function AdminLoginPage() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = () => {
+    // eslint-disable-next-line no-console
+    console.log('로그인', { email, password })
+    // TODO: API 연동 예정
+  }
+
+  const inputStyle =
+    'rounded-md border-[#bdbdbd] font-[14px] text-[#bdbdbd] w-82 h-12 tracking-[-3%] placeholder:text-sm px-2 py-1 focus:placeholder:opacity-0'
+
+  return (
+    <section className="flex h-screen bg-primary-500/3">
+      <div className="flex h-full w-1/2 flex-col items-center justify-center bg-white">
+        <Image
+          src={OzcodingLogo}
+          alt="오즈코딩 로고"
+          className="mb-4 h-6 w-45"
+        />
+        <p className="mb-4 text-sm">
+          <span className="text-primary-500">admin 계정</span>
+          <span className="text-whilte">을 통해 로그인을 진행해주세요.</span>
+        </p>
+        <BaseInput
+          className={cn(inputStyle, 'mb-3')}
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="아이디 (example@gmail.com"
+        />
+        <BaseInput
+          className={cn(inputStyle, 'mb-3')}
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="비밀번호 (6~15자의 영문 대소문자, 숫자, 특수문자 포함)"
+        />
+        <Button className="h-12 w-82" variant="primary" onClick={handleSubmit}>
+          로그인
+        </Button>
+      </div>
+      <div className="w-1/2" />
+    </section>
+  )
+}
