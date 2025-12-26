7d09027bbf94d90d3f49e1f2da7a7b200ac4dba7
Sammylee <sammul95@gmail.com>
2025-12-27 00:27:49 +0900

    refactor: 어드민 로그인페이지 ui 리팩토링(#95)
    - form 태그로 변경 후 버튼 타입 submit으로 변경
    - 오타 수정

--- src/pages/AdminLoginPage.tsx
+++ src/pages/AdminLoginPage.tsx
@@ -1,5 +1,5 @@
 import { OzcodingLogo } from '@assets'
-import { BaseInput, Button, Image, showToast } from '@components'
+import { BaseInput, Button, Image } from '@components'
 import { useLogin } from '@features/auth/hooks/useLogin'
 import { cn } from '@utils'
 import { useState } from 'react'
@@ -8,40 +8,36 @@ export default function AdminLoginPage() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const loginMutaion = useLogin()
+  const loginMutation = useLogin()
 
-  const handleSubmit = () => {
-    console.log('로그인성공', { email, password })
-    if (!email || !password) {
-      showToast('이메일과 비밀번호를 입력해주세요')
-
-      return
-    }
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
     // mutate 실행 -> onSuccess/onError 자동처리
-    loginMutaion.mutate({ email, password })
+    loginMutation.mutate({ email, password })
   }
 
   const inputStyle =
     'rounded-md border-[#bdbdbd] font-[14px] text-[#bdbdbd] w-82 h-12 tracking-[-3%] placeholder:text-sm px-2 py-1 focus:placeholder:opacity-0'
 
   return (
-    <section className="flex h-screen bg-primary-500/3">
+    <form onSubmit={handleSubmit} className="flex h-screen bg-primary-500/3">
       <div className="flex h-full w-1/2 flex-col items-center justify-center bg-white">
         <Image
           src={OzcodingLogo}
           alt="오즈코딩 로고"
           className="mb-4 h-6 w-45"
+          loading="eager"
         />
         <p className="mb-4 text-sm">
           <span className="text-primary-500">admin 계정</span>
-          <span className="text-whilte">을 통해 로그인을 진행해주세요.</span>
+          <span className="text-white">을 통해 로그인을 진행해주세요.</span>
         </p>
         <BaseInput
           className={cn(inputStyle, 'mb-3')}
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          placeholder="아이디 (example@gmail.com"
+          placeholder="아이디 (example@gmail.com)"
         />
         <BaseInput
           className={cn(inputStyle, 'mb-3')}
@@ -50,11 +46,16 @@ export default function AdminLoginPage() {
           onChange={(e) => setPassword(e.target.value)}
           placeholder="비밀번호 (6~15자의 영문 대소문자, 숫자, 특수문자 포함)"
         />
-        <Button className="h-12 w-82" variant="primary" onClick={handleSubmit}>
+        <Button
+          className="h-12 w-82"
+          variant="primary"
+          type="submit"
+          disabled={loginMutation.isPending}
+        >
           로그인
         </Button>
       </div>
       <div className="w-1/2" />
-    </section>
+    </form>
   )
 }
