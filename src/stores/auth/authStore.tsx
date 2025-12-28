70fd3784440b921ad86d1348b343640eafe435c0
Sammylee <sammul95@gmail.com>
2025-12-29 02:33:57 +0900

    refactor: 폴더구조 정리(#106)
    - stores/auth 중복 폴더 삭제
    - store → stores 폴더명 변경
    - features/exams/questions → features/exams/exam-managements/questions 이동

--- src/stores/auth/authStore.tsx
+++ src/stores/auth/authStore.tsx
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 
-export type AuthState = {
+type AuthState = {
   accessToken: string | null
   isAuthenticated: boolean
 
