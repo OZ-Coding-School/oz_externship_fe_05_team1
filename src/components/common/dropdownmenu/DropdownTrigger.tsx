4265757c04efd4bef00ed6d5fe9a91adaf044df1
LeeSaemMool <sammul95@gmail.com>
2025-12-30 10:19:30 +0900

    fix: 코드리뷰 반영(#127)

--- src/components/common/dropdownmenu/DropdownTrigger.tsx
+++ src/components/common/dropdownmenu/DropdownTrigger.tsx
@@ -23,7 +23,6 @@ export const DropdownTrigger = ({
   size = 'md',
 }: DropdownTriggerProps) => (
   <button
-    type="button"
     ref={buttonRef}
     onClick={onClick}
     aria-expanded={isOpen}
