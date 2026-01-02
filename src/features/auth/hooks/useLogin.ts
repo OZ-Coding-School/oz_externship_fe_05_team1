4614ad0e199a4dfadeea2c9c9e0112399d593610
Sammylee <sammul95@gmail.com>
2026-01-02 23:55:46 +0900

    fix: 로그인 인증 유지 및 캐시 문제 수정 (#148)

--- src/features/auth/hooks/useLogin.ts
+++ src/features/auth/hooks/useLogin.ts
@@ -4,17 +4,21 @@ import { login } from '@api/auth'
 import { showToast } from '@components'
 import { ROUTES_PATHS } from '@constants'
 import { useAuthStore } from '@stores'
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useNavigate } from 'react-router-dom'
 
 export function useLogin() {
   const navigate = useNavigate()
+  const queryClient = useQueryClient()
   const setAccessToken = useAuthStore((state) => state.setAccessToken)
 
   return useMutation({
     mutationFn: (data: LoginRequest) => login(data),
     onSuccess: (data: LoginResponse) => {
+      queryClient.clear()
+
       setAccessToken(data.access_token)
+
       showToast('로그인성공', 'success')
       navigate(ROUTES_PATHS.MAIN)
     },
