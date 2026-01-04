0a29222dac2921a1c2b904c251fe6f68c7d1fe0b
jwb <jeonwb622@gmail.com>
2026-01-05 02:04:19 +0900

    fix: 쪽지시험 생성 모달 디자인 버그 수정(#171)
    - 이미지 이름 길이가 일정 이상 넘지 않게 수정
    - 쪽지시험 시험삭제 버튼 클릭 시 목록조회 및 리프래쉬 되도록 수정
    - 쪽지시험 생성 버튼 클릭 시 이전 내용 초기화 되도록 수정

--- src/features/exams/queries/useExamDeleteMutation.ts
+++ src/features/exams/queries/useExamDeleteMutation.ts
@@ -1,16 +1,25 @@
 import { deleteExamRequest } from '@api/exams'
 import { showToast } from '@components'
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
-export const useExamDeleteMutation = (onClose: () => void) =>
-  useMutation({
+export const useExamDeleteMutation = (onDeleted: () => void) => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
     mutationFn: (examId: number) => deleteExamRequest(examId),
 
-    onSuccess: () => {
+    onSuccess: async () => {
       showToast('시험이 삭제되었습니다.', 'success')
 
-      return onClose()
+      onDeleted()
+
+      await queryClient.invalidateQueries({
+        queryKey: ['exams'],
+      })
     },
 
-    onError: () => showToast('시험 삭제 중 오류가 발생했습니다.', 'fail'),
+    onError: () => {
+      showToast('시험 삭제 중 오류가 발생했습니다.', 'fail')
+    },
   })
+}
